15bb674c0301fe5460ae7fe08974b5398d0c7909
Nicholas Horton <nicholasjhorton@gmail.com>
2024-03-10 19:20:28 -0400

    More work on documentation.

--- R/data.R
+++ R/data.R
@@ -1,14 +1,33 @@
-#' My data
+#' Conversion Factors
 #'
-#' This is more descriptive text about my data.
+#' XX This is more descriptive text about my data.
 #' Even more info is provided here ...
 #'
-#' @format ## `foo`
+#' @format ## `ConversionFactor`
 #' A data frame with XX rows and XX columns:
 #' \describe{
-#'   \item{lines}{lines of text}
-#'   \item{author}{who wrote that line}
+#'   \item{food_id}{XX}
+#'   \item{measure_id}{XX}
+#'   \item{conversion_factor_value}{XX}
+#'   \item{conv_factor_date_of_entry}{XX}
 #'   ...
 #' }
-#' @source <https://www.who.int/teams/global-tuberculosis-programme/data>
-"foo"
+#' @source <https://www.canada.ca/en/health-canada/services/food-nutrition/healthy-eating/nutrient-data/canadian-nutrient-file-2015-download-files.html>
+"ConversionFactor"
+
+#' Food Groups
+#'
+#' XX This is more descriptive text about my data.
+#' Even more info is provided here ...
+#'
+#' @format ## `FoodGroup`
+#' A data frame with XX rows and XX columns:
+#' \describe{
+#'   \item{food_group_id}{XX}
+#'   \item{food_group_code}{XX}
+#'   \item{food_group_name}{XX}
+#'   \item{food_group_name_f}{XX}
+#'   ...
+#' }
+#' @source <https://www.canada.ca/en/health-canada/services/food-nutrition/healthy-eating/nutrient-data/canadian-nutrient-file-2015-download-files.html>
+"FoodGroup"

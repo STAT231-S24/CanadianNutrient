911ae7f08f3c587cbb42a19fe98f9d76259c0bfb
Javier Gutierrez Bach <javier.gutierrez.bach@gmail.com>
2024-03-26 13:26:10 -0400

    Update DATASET.R
    Added the loading in of all the tables, might need to check if there is some wrangling to do in the data but it looks promising.

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -13,6 +13,56 @@ FoodGroup <-
   janitor::clean_names() 
 usethis::use_data(FoodGroup)  
 
+FoodNames <-  
+  readr::read_csv("data-raw/FOOD_NAME.csv") |>
+  janitor::clean_names() 
+usethis::use_data(FoodNames)  
+
+FoodSources <-  
+  readr::read_csv("data-raw/FOOD_SOURCE.csv") |>
+  janitor::clean_names() 
+usethis::use_data(FoodSources)  
+
+MeasureNames <-  
+  readr::read_csv("data-raw/MEASURE_NAME.csv") |>
+  janitor::clean_names() 
+usethis::use_data(MeasureNames)  
+
+NutrientAmounts <-  
+  readr::read_csv("data-raw/NUTRIENT_AMOUNT.csv") |>
+  janitor::clean_names() 
+usethis::use_data(NutrientAmounts) 
+
+NutrientNames <-  
+  readr::read_csv("data-raw/NUTRIENT_NAME.csv") |>
+  janitor::clean_names() 
+usethis::use_data(NutrientNames) 
+
+NutrientSources <-  
+  readr::read_csv("data-raw/NUTRIENT_SOURCE.csv") |>
+  janitor::clean_names() 
+usethis::use_data(NutrientSources) 
+
+RefuseAmounts <-  
+  readr::read_csv("data-raw/REFUSE_AMOUNT.csv") |>
+  janitor::clean_names() 
+usethis::use_data(RefuseAmounts) 
+
+RefuseNames <-  
+  readr::read_csv("data-raw/REFUSE_NAME.csv") |>
+  janitor::clean_names() 
+usethis::use_data(RefuseNames) 
+
+YieldAmounts <-  
+  readr::read_csv("data-raw/YIELD_AMOUNT.csv") |>
+  janitor::clean_names() 
+usethis::use_data(YieldAmounts) 
+
+YieldNames <-  
+  readr::read_csv("data-raw/YIELD_NAME.csv") |>
+  janitor::clean_names() 
+usethis::use_data(YieldNames) 
+
 # CONVERSION_FACTOR.csv
 # FOOD_GROUP.csv
 # FOOD_NAME.csv

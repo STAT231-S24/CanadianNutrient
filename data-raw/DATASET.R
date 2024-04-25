e569fff1127f47b40d3ad8bd6a5b205fe716c5e4
Javier Gutierrez Bach <javier.gutierrez.bach@gmail.com>
2024-04-25 13:07:33 -0400

    update

--- data-raw/DATASET.R
+++ data-raw/DATASET.R
@@ -10,21 +10,18 @@ usethis::use_data(ConversionFactor, overwrite = TRUE)
 
 FoodGroup <-  
   readr::read_csv("data-raw/FOOD_GROUP.csv") |>
-  janitor::clean_names() |> 
-  select(-food_group_name_f)
+  janitor::clean_names() 
 usethis::use_data(FoodGroup, overwrite = TRUE)  
 
 FoodNames <-  
   readr::read_csv("data-raw/FOOD_NAME.csv") |>
-  janitor::clean_names() |> 
-  select(-food_description_f)
+  janitor::clean_names() 
 usethis::use_data(FoodNames, overwrite = TRUE)  
 
 
 FoodSources <-  
   readr::read_csv("data-raw/FOOD_SOURCE.csv") |>
-  janitor::clean_names() |> 
-  select(-food_source_description_f)
+  janitor::clean_names() 
 usethis::use_data(FoodSources, overwrite = TRUE)  
 
 MeasureNames <-  
@@ -39,14 +36,12 @@ usethis::use_data(NutrientAmounts, overwrite = TRUE)
 
 NutrientNames <-  
   readr::read_csv("data-raw/NUTRIENT_NAME.csv") |>
-  janitor::clean_names() |> 
-  select(-nutrient_name_f)
+  janitor::clean_names() 
 usethis::use_data(NutrientNames, overwrite = TRUE) 
 
 NutrientSources <-  
   readr::read_csv("data-raw/NUTRIENT_SOURCE.csv") |>
-  janitor::clean_names() |> 
-  select(-nutrient_sourc_description_f)
+  janitor::clean_names() 
 usethis::use_data(NutrientSources, overwrite = TRUE) 
 
 RefuseAmounts <-  
@@ -56,8 +51,7 @@ usethis::use_data(RefuseAmounts, overwrite = TRUE)
 
 RefuseNames <-  
   readr::read_csv("data-raw/REFUSE_NAME.csv") |>
-  janitor::clean_names() |> 
-  select(-refuse_description_f)
+  janitor::clean_names() 
 usethis::use_data(RefuseNames, overwrite = TRUE) 
 
 YieldAmounts <-  
@@ -67,8 +61,7 @@ usethis::use_data(YieldAmounts, overwrite = TRUE)
 
 YieldNames <-  
   readr::read_csv("data-raw/YIELD_NAME.csv") |>
-  janitor::clean_names() |> 
-  select(-yield_description_f)
+  janitor::clean_names() 
 usethis::use_data(YieldNames, overwrite = TRUE) 
 
 # CONVERSION_FACTOR.csv
